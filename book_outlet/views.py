8a4ff302259b263f2787d79504037bdc4163e542
Engineer-kim <ddang0103@naver.com>
2024-11-06 22:04:15 +0900

    Feature: URL 패턴 변경의 용이하기 위한 get_absolute_url 사용  - 메서드를 통한 URL 하드코딩을 하지않고 일관성이 유지되도록함

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,23 +1,24 @@
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Book
+from django.db.models   import Avg, Max,Min
 
 # Create your views here.
 
 
 def index(request):
 
-    books = Book.objects.all()
+    books = Book.objects.all().order_by('-rating') # -rating ==>  rating 내림 차순 정렬
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg('rating'))
     return render(request, 'book_outlet/index.html' ,{
-        'books': books
+        'books': books,
+        "total_number_of_books": num_books,
+        "average_rating": avg_rating,
     })
 
-def book_detail(request, id):
-#    try:
-
-#    except Book.DoesNotExist:
-#       raise Http404("Book not found")
-    book = get_object_or_404(Book, pk=id)
+def book_detail(request, slug):
+    book = get_object_or_404(Book, slug=slug)
     return render(request, 'book_outlet/book_detail.html',{
         'title': book.title,
         'author': book.author,
