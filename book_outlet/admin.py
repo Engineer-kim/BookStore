65bb0726d4f3f409915826d63fb6894d554cc96a
Engineer-kim <ddang0103@naver.com>
2024-11-07 18:55:45 +0900

    Feature: 사용안하는 함수 및 어드민페이지내의 필터와 같은 UI 변경

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -4,4 +4,10 @@ from .models import Book
 
 # Register your models here.
 
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    #readonly_fields = ("slug",) #Model.py의 슬러그 속성에 대한  수정 불가 기능 추가와 같음
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("author" ,"rating") # 필터링을 통한 데이터 분류 할수있게 하는 기능
+    list_display = ("title", "author") # 데이터에 대한 속성 이름 표시
+
+admin.site.register(Book , BookAdmin)
